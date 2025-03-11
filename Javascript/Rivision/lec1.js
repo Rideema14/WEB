9b35fc3132eaf05b0c02c532fef0544c87092130
Rideema14 <rideemasinghjbp@gmail.com>
2025-03-11 19:29:34 +0530

    node

---
+++ Javascript/Rivision/lec1.js
@@ -0,0 +1,34 @@
+console.log("rideema")
+console.log("hello")
+console.log("byeee")
+//We know about console.log()
+var rideema = 14;
+console.log(rideema);
+//  caseing
+var SmallNumber=1//pascel
+var small_number=1//snake
+var smallNumberWithClass=2 //camel
+var small_number=5
+small_number=4
+console.log(small_number);
+  
+// const otherNumber=10 // camel
+// otherNumber=11
+// console.log(otherNumber);
+//error
+ let OtherNumber= 20//pascel
+ OtherNumber=30
+ console.log(OtherNumber);
+  //Arthemetic operation +,-,*,
+  const num1=10
+  const num2=20
+  const num3=30
+  console.log(num1+num2+num3);
+  console.log(num1-num2-num3);
+  console.log(num1/num2/num3);
+  console.log(num1%num2%num3);
+  console.log(num1*num2*num3);
+  
+  
+ 
+
