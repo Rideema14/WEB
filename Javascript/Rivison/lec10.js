eedcb7d74f8b368cf14608e58da0ab6aea1929e8
Rideema14 <rideemasinghjbp@gmail.com>
2025-05-29 21:30:55 +0530

    code

---
+++ Javascript/Rivison/lec10.js
@@ -0,0 +1,26 @@
+var a=1
+function outer(){
+    console.log(a)
+    var a=2
+    function inner(){
+        console.log(a)
+    }
+    inner()
+}
+outer()
+
+function foo(){
+    function bar(){
+        return 3
+    }
+    return bar()
+    function bar(){
+        return 8
+    }
+}
+console.log(foo())
+//gobal and scope varibales
+if(false){
+    var a=5
+}
+console.log(a)
